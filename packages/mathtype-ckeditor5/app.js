25751a8d25189384dbeb4ffb65d23e343b681ba5
Xavier Ripoll <xavier@wiris.com>
2020-01-20 11:18:47 +0100

    Modify CKEditor 5 to include demo

---
+++ packages/mathtype-ckeditor5/app.js
@@ -0,0 +1,20 @@
+// app.js
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import MathType from './src/plugin';
+
+ClassicEditor
+    .create( document.querySelector( '#editor' ), {
+        plugins: [ Essentials, Paragraph, Bold, Italic, MathType ],
+        toolbar: [ 'bold', 'italic', 'MathType', 'ChemType' ]
+    } )
+    .then( editor => {
+        console.log( 'Editor was initialized', editor );
+    } )
+    .catch( error => {
+        console.error( error.stack );
+    } );
---
+++ packages/mathtype-ckeditor5/instructions.md
@@ -0,0 +1,17 @@
+How to run the demo:
+
+1. In the root of the project (`html-integrations/`), run:
+
+```sh
+npm install
+lerna bootstrap
+```
+
+2. In `html-integrations/packages/mathtype-ckeditor5/`, run:
+
+```sh
+npm run build
+npm run serve
+```
+
+3. Connect to the demo on `http://127.0.0.1:8080/`.
